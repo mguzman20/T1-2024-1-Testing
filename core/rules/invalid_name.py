2dbb2ae49791c521296da4b5a72b579bd0aeef42
JOSÉ MANUEL JOUANNE <jmjouanne@uc.cl>
2024-04-05 12:47:12 -0300

    1, 3 y 5

--- core/rules/invalid_name.py
+++ core/rules/invalid_name.py
@@ -1,3 +1,5 @@
+from _ast import FunctionDef, ClassDef
+from typing import Any
 from ..rule import *
 
 # Clases que permiten detectar el uso de un nombre invalido en clases, metodos y funciones
@@ -7,11 +9,43 @@ class InvalidNameVisitor(WarningNodeVisitor):
     def __init__(self):
         super().__init__()
 
+    def visit_FunctionDef(self, node: FunctionDef):
+        if not name_is_snake_case(node.name):
+            if node.args.args[0].arg == 'self':
+                self.addWarning('InvalidName', node.lineno, 'invalid method name ' + node.name)
+            else:
+                self.addWarning('InvalidName', node.lineno, 'invalid function name ' + node.name)
+        NodeVisitor.generic_visit(self, node)
+
+    def visit_ClassDef(self, node: ClassDef):
+        if not name_is_camel_case(node.name):
+            self.addWarning('InvalidName', node.lineno, 'invalid class name ' + node.name)
+        NodeVisitor.generic_visit(self, node)
+
 
 class InvalidNameRule(Rule):
     def analyze(self, ast):
-        pass
+        visitor = InvalidNameVisitor()
+        visitor.visit(ast)
+        return visitor.warningsList()
     
     @classmethod
     def name(cls):
         return 'invalid-name'
+
+
+def name_is_snake_case(name: str):
+    if name[0].islower() and len(name) < 32 and len(name) > 2:
+        for char in name:
+            if not char.isdigit() and not char.isalpha() and char != '_':
+                return False
+        return True
+    return False
+
+def name_is_camel_case(name: str):
+    if name[0].isupper() and len(name) > 0:
+        for char in name:
+            if not char.isdigit() and not char.isalpha() and char != '_':
+                return False
+        return True
+    return False
--- core/transformers/operator_equals_rewriter.py
+++ core/transformers/operator_equals_rewriter.py
@@ -1,4 +1,6 @@
+from _ast import AST, Assign
 from ast import *
+from typing import Any
 from core.rewriter import RewriterCommand
 
 # Clases que permiten transformar codigo que contiene x = x <operador_aritmetico_binario> z a x <operador_aritmetico_binario>= z.
@@ -8,8 +10,27 @@ class OperatorEqualsTransformer(NodeTransformer):
     def __init__(self):
         super().__init__()
 
+    def visit_Assign(self, node: Assign):
+        if isinstance(node.value, BinOp):
+            if isinstance(node.value.right, Name):
+                if node.value.right.id == node.targets[0].id:
+                    # AugAssign
+                    return AugAssign(
+                        target=node.targets[0],
+                        op=node.value.op,
+                        value=node.value.left
+                    )
+            if isinstance(node.value.left, Name):
+                if node.value.left.id == node.targets[0].id:
+                    return AugAssign(
+                        target=node.targets[0],
+                        op=node.value.op,
+                        value=node.value.right
+                    )
+
 
 class OperatorEqualsCommand(RewriterCommand):
 
     def apply(self, ast):
-        pass
+        new_tree = fix_missing_locations(OperatorEqualsTransformer().visit(ast))
+        return new_tree
